4212567cac435b91348585b72d968f47277cf102
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-02-16 09:48:46 +0100

    fixed type text

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -20,7 +20,7 @@
                         <h2 class="card-title">
                             {{$project->title}}
                         </h2>
-                        type = <strong><{{ $project->type->name }}</strong>
+                        type <strong>{{ $project->type->name }}</strong>
                         <img src="{{$project->img_url}}" class="card-img-top" alt="project image">
                         <div class="card-body">
                             <h4 class="card-title">Date: {{$project->date}} </h4>
